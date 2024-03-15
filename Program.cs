d71b8517f1496cc941c3fcbf623c4c3b5ecd94ee
Aimira <aimira.abdrahman@gmail.com>
2024-03-15 18:53:13 +0500

    Added draft solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] GetFinalArrayWithSelectedElements(string input)
+{
+    string[] InitialArray = input.Split(" ");
+    string[] FinalArray = new string[InitialArray.Length];
+    for (int i = 0; i < InitialArray.Length; i++)
+    {
+        if (InitialArray[i].Length <= 3)
+        {
+            FinalArray[i] = InitialArray[i];
+        }
+    }
+    return FinalArray;
+}
+
+Console.Write("Введите строки через пробел: ");
+string inputStr = Console.ReadLine();
+string[] result = GetFinalArrayWithSelectedElements(inputStr);
+Console.WriteLine($"Массив: [{string.Join("; ", result)}]");
+
+
