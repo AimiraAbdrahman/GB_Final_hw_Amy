72376677e71ee2f84131139e6eab70d8852835ba
Aimira <aimira.abdrahman@gmail.com>
2024-03-22 13:13:22 +0500

    Added final solution

--- Program.cs
+++ Program.cs
@@ -1,17 +1,16 @@
 ﻿string[] GetFinalArrayWithSelectedElements(string input)
 {
-    string[] InitialArray = input.Split(" ");
-    string[] FinalArray = new string[InitialArray.Length];
-    for (int i = 0; i < InitialArray.Length; i++)
-    {
-        if (InitialArray[i].Length <= 3)
-        {
-            FinalArray[i] = InitialArray[i];
-        }
-    }
-    return FinalArray;
+string[] InitialArray = input.Split(" ");
+string[] FinalArray = new string[InitialArray.Length];
+for (int i = 0; i < InitialArray.Length; i++)
+{
+if (InitialArray[i].Length <= 3)
+{
+FinalArray[i] = InitialArray[i];
 }
-
+}
+return FinalArray;
+}
 Console.Write("Введите строки через пробел: ");
 string inputStr = Console.ReadLine();
 string[] result = GetFinalArrayWithSelectedElements(inputStr);
--- obj/Debug/net8.0/Final_hw.AssemblyInfo.cs
+++ obj/Debug/net8.0/Final_hw.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final_hw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7362eaf53a26d0b036dfd223ac957e89e2af0ca7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d71b8517f1496cc941c3fcbf623c4c3b5ecd94ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final_hw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final_hw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
